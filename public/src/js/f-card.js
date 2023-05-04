2532e855ee07a514922316bed12f2cf8f527f518
Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-05-04 09:10:38 +0000

    face book plugin added

--- public/src/js/f-card.js
+++ public/src/js/f-card.js
@@ -2,17 +2,16 @@
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
-let interval = null;
 
-const screen = document.querySelector(".f-card-in"),
-      name = document.querySelector(".note");
+const screen = document.querySelector(".f-card-in");
+const  name = document.querySelector(".note");
 
 screen.onmouseenter = event => {  
   let iteration = 0;
   
   clearInterval(interval);
   
-  interval = setInterval(() => {
+  var interval = setInterval(() => {
     name.innerText = name.innerText
       .split("")
       .map((letter, index) => {
@@ -31,3 +30,45 @@ screen.onmouseenter = event => {
     iteration += 1 ;
   }, 30);
 }
+
+let isAnimating = false;
+
+function animateName() {
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    name.innerText = name.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return name.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 27)]
+      })
+      .join("");
+    
+    if(iteration >= name.dataset.value.length){ 
+      clearInterval(interval);
+      isAnimating = false;
+    }
+    
+    iteration += 1 ;
+  }, 30);
+}
+
+function handleScroll() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  const middlePosition = scrollTop + (clientHeight / 2);
+  
+  if (middlePosition >= screen.offsetTop && middlePosition <= (screen.offsetTop + screen.offsetHeight)) {
+    if (!isAnimating) {
+      animateName();
+      isAnimating = true;
+    }
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
