c35b25b05f06b5d50e4b6b80e697e4781ed9f1d0
Rabbi Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-06-07 16:39:57 +0000

    somwhow

--- public/src/js/gallery.js
+++ public/src/js/gallery.js
@@ -1,21 +1,277 @@
 const imagesData = [
     {
-        src: "src/img/sandy4.jpg",
-        title: "sandy",
-        description: "Class aptent taciti sociosqu ad litora torquentper conubia nostra, per inceptos himenaeo",
+        src: "src/img/sandy1.jpg",
+        title: "Sandy's epic black and white",
+        description: "I am not really sure where this picture was taken, but i guess it was intended for passport pic ",
+        dialogSrc: "src/img/sandy1.jpg",
+        dialogText: "This portrait features Sandra with short, braided hair. She is standing in an indoor setting, leaning against a wall and looking directly at the camera. Her facial expression is neutral and her eyes are focused on the viewer. The woman has full lips, arched eyebrows, and long eyelashes that frame her face nicely. Her neck is visible below her chin as she looks up towards the camera lens.\n" +
+            "\n" +
+            "The woman's outfit consists of a black top with white accents around the collar and sleeves. She wears minimal makeup which allows for her natural beauty to shine through in this portrait photograph. The background of this image is mostly white with some grey tones present throughout it as well as some black elements near the bottom right corner of the photo."
+    },
+    {
+        src: "src/img/sandy2.jpg",
+        title: "Dreadlocks Pic",
+        description: "This is one for my favorite, I love this deadlocks",
         dialogSrc: "src/img/sandy2.jpg",
-        dialogText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry..."
+        dialogText: "Sandra with dreadlocks is standing outdoors, looking directly at the camera. She is wearing a white t-shirt and a brown belt around her waist. Her eyebrows are raised slightly as she takes a selfie, her face illuminated by the sun. Her hair is styled in long braids that hang down to her shoulders, framing her face and accentuating her features. The background of the image consists of trees and foliage, providing an earthy contrast to the bright colors of the girl's clothing and accessories. This portrait captures a moment of self-expression for this young woman - one that will be remembered for years to come!"
+    },
+    {
+        src: "src/img/sandy3.jpg",
+        title: "smiling widely at outdoor",
+        description: "Her checks like something I won't say",
+        dialogSrc: "src/img/sandy3.jpg",
+        dialogText: "This image shows Sandra with short hair, standing outdoors in front of a tree. The person is wearing a white shirt and has an expression of joy on their face, as they are smiling widely. They have dark black hair that is cut short and styled neatly around the sides of their head. Their eyes are closed slightly, giving them an even more cheerful look.\n" +
+            "\n" +
+            "The background consists mostly of green foliage from the trees surrounding them, along with some patches of blue sky visible through the branches above. The sun is shining brightly down on this scene, creating highlights across the person's clothing and skin tone which gives it a warm glow overall.\n" +
+            "\n" +
+            "The colors used in this image are mainly shades of green for the foliage and blue for the sky, while also featuring darker tones such as black for the person's hair and lighter ones like white for their shirt. There is also some yellow present from sunlight reflecting off certain areas in this picture."
+    },
+    {
+        src: "src/img/sandy4.jpg",
+        title: "Arms crossed",
+        description: "Miss style has crossed her arm with a slightly smiling on her face.",
+        dialogText: "This image shows Sandra with long black braids, wearing a blue shirt and standing outdoors. She has her arms crossed in front of her chest and is smiling slightly. Her shirt has short sleeves that end just above the elbow, revealing her toned forearms. The background of the photo is blurred out, but it appears to be an outdoor setting with grassy ground beneath her feet. The woman's face is in focus and she looks directly at the camera with a warm expression on her face. Her eyes are bright and alert, while her lips curl up into a slight smile that conveys both confidence and joy."
+    },
+    {
+        src: "src/img/sandy5.jpg",
+        title: "Frame Sandy",
+        description: "So why did you stick your head into the frames, what if it get stacks",
+        dialogText: "This image depicts a Sandra, approximately 7 years old, taking a selfie indoors. She is wearing a black head covering and has her lips slightly sticking out in an expression of joy or amusement. Her clothing is mostly monochrome and she appears to be smiling broadly. The background of the photo is grey with no other objects visible in the frame.\n" +
+            "\n" +
+            "The girl's face takes up most of the frame, with her eyes looking directly at the camera lens as if she was aware that she was being photographed. Her skin tone is light and her hair color matches that of her head covering which gives off an overall dark hue to the photograph.\n" +
+            "\n" +
+            "The lighting in this image appears to be natural, coming from either side of the subject rather than from behind or above them; it casts shadows on both sides of their face but does not obscure any features or details about them. This creates a sense of depth within the photograph while still keeping all elements clearly visible for viewers to appreciate its beauty and composition."
+    },
+    {
+        src: "src/img/sandy6.jpg",
+        title: "Two sisters posing for a photo",
+        description: "so this sister of yours what her name again",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy7.jpg",
+        title: "Ei you look like Leon Thomas III",
+        description: "My girl is now Leon Thomas III smiling and wearing a dress",
+        dialogText: "This image features Leon Thomas III, which is actually my Sandra, female celebrity with braids and a bright smile. She is wearing a dress outdoors in front of an unknown building. The sun is shining on her face, highlighting her beautiful features and making her skin glow. Her eyes are closed as she smiles widely, showing off her pearly white teeth. Her hair is styled in two long braids that hang down to the middle of her back.\n" +
+            "\n" +
+            "The background of the photo consists of grey pavement and green grass surrounding the building behind Leon Thomas III. There appears to be some kind of fence or wall near the bottom right corner of the image, but it's hard to make out due to its blurriness. In addition, there are several trees visible in the distance at various points throughout the picture which adds depth and texture to this outdoor scene.\n" +
+            "\n" +
+            "Overall, this photograph captures an uplifting moment between Leon Thomas III and nature - one that radiates joyfulness from both subject and setting alike!"
+    },
+    {
+        src: "src/img/sandy8.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy9.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy10.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy11.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy12.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy13.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy14.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy15.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy16.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy17.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy18.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy19.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy20.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy21.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy22.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy23.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy24.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy25.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy26.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
     },
     {
         src: "src/img/sandy27.jpg",
-        title: "happy bdad",
-        description: "Class aptent taciti sociosqu ad litora torquentper conubia nostra, per inceptos himenaeo",
-        dialogSrc: "src/img/sandy20.jpg",
-        dialogText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry..."
-    }
-    // Add more objects for more images and captions
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy28.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy29.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy30.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy31.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy32.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy33.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy34.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy35.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    }, {
+        src: "src/img/sandy36.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy37.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy38.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy39.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy40.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy41.jpg",
+        title: "",
+        description: "",
+        dialogText: ""
+    },
+    {
+        src: "src/img/sandy42.jpg",
+        title: "Eii is something wrong",
+        description: "The last time i check i never said i will leave you",
+        dialogText: "Remove you hands from your chin fast and smile 😁"
+    },
+
+
 ];
 
+let i = 0;
 function createImageElement(data) {
     // Create the necessary elements
     const a = document.createElement("a");
@@ -27,7 +283,7 @@ function createImageElement(data) {
     const p = document.createElement("p");
 
     // Set the attributes and content for the elements
-    a.href = "#small-dialog";
+    a.href = `#small-dialog${i}`;
     a.classList.add("thickbox", "play-icon", "popup-with-zoom-anim");
     divWorkColumn.classList.add("work-column");
     img.src = data.src;
@@ -49,9 +305,44 @@ function createImageElement(data) {
     return a;
 }
 
+function createSubElement(data){
+    //small-dialog
+    const divSmallDialog = document.createElement('div');
+    const divImageTop = document.createElement('div');
+    const img2 = document.createElement('img');
+    const p2 = document.createElement('p');
+
+    // Set attribute and content for the elements
+    divSmallDialog.classList.add("mfp-hide");
+    divSmallDialog.id = `small-dialog${i}`;
+    i++;
+    divImageTop.classList.add("image-top")
+    img2.src = data.src;
+    img2.alt = '';
+    p2.innerText = data.dialogText;
+
+    // Assemble the elements
+    divSmallDialog.appendChild(divImageTop);
+    divImageTop.appendChild(img2);
+    divImageTop.appendChild(p2);
+
+    return divSmallDialog;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 const parentElement = document.querySelector(".row.work-row");
+
+shuffleArray(imagesData);
 
 imagesData.forEach((data) => {
     const imageElement = createImageElement(data);
+    const smallDialog = createSubElement(data);
     parentElement.appendChild(imageElement);
+    parentElement.appendChild(smallDialog);
 });
